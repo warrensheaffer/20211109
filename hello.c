89b547b140628d0b67fb71f7b264b6c79a536aa8
Warren <Warren.Sheaffer@yahoo.com>
2021-11-09 13:12:47 -0600

    Added helllo.c
    Sample code to git

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*
+ * Program to add to the git repository
+ * Author: Warren Sheaffer
+ * Date: 11/9/21
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World!\n");
+	return 0;
+
+} // main
